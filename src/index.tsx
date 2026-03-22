a9308cf67842afc2e940a4e2ab5fb95090a616cb
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2026-03-22 12:05:22 +0100

    feat: cleanup, tailwindcss, html format

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import "./index.css"
-import App from "./App.tsx"
+import "./global.css"
 
 // biome-ignore lint/style/noNonNullAssertion: react setup
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <div className="bg-black text-white mx-10 my-3">Hello</div>
   </StrictMode>
 )
